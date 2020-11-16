a1da6e7585320d038076f029e6069a4dd5ea3376
MSzostaq <maciek.szostak55@gmail.com>
2020-11-16 10:01:47 +0100

    creating sidebar

--- src/components/molecules/Card/Card.jsx
+++ src/components/molecules/Card/Card.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled, { css } from "styled-components";
 import Button from "../../atoms/Button/Button";
 import Heading from "../../atoms/Heading/Heading";
+import LinkIcon from "../../../assets/icons/link.svg";
 import Paragraph from "../../atoms/Paragraph/Paragraph";
 
 const StyledWrapper = styled.div`
@@ -15,8 +17,14 @@ const StyledWrapper = styled.div`
 
 const InnerWrapper = styled.div`
   padding: 17px 30px;
-  background-color: ${({ yellow, theme }) =>
-    yellow ? theme.primary : "#ffffff"};
+  position: relative;
+  background-color: ${({ activeColor, theme }) =>
+    theme[activeColor] ? theme.primary : "#ffffff"};
+
+  :first-of-type {
+    z-index: 99;
+  }
+
   ${({ flex }) =>
     flex &&
     css`
@@ -36,17 +44,50 @@ const StyledHeading = styled(Heading)`
   margin 5px 0 0;
 `;
 
-const Card = () => (
+const StyledAvatar = styled.img`
+  border: 5px solid ${({ theme }) => theme.twitter};
+  border-radius: 50px;
+  position: absolute;
+  right: 25px;
+  top: 25px;
+  width: 86px;
+  height: 86px;
+`;
+
+const StyledLinkButton = styled.a`
+  background: "#ffffff" url(${LinkIcon}) no-repeat;
+  background-position: 50%;
+  background-size: contain;
+  border-radius: 50px;
+  display: block;
+  position: absolute;
+  right: 25px;
+  top: 25px;
+  width: 47px;
+  height: 47px;
+`;
+
+const Card = ({ cardType }) => (
   <StyledWrapper>
-    <InnerWrapper>
-      <StyledHeading>Helloo</StyledHeading>
+    <InnerWrapper activeColor={cardType}>
+      <StyledHeading>Hello Roman</StyledHeading>
       <DateInfo>4 days</DateInfo>
+      <StyledAvatar />
+      <StyledLinkButton />
     </InnerWrapper>
     <InnerWrapper>
-      <Paragraph>my Paragraphworks</Paragraph>
+      <Paragraph>my Paragraph works</Paragraph>
       <Button secondary>Remove</Button>
     </InnerWrapper>
   </StyledWrapper>
 );
+
+Card.propTypes = {
+  cardType: PropTypes.oneOf(["note", "twitter", "article"]),
+};
+
+Card.defaulrProps = {
+  cardType: "note",
+};
 
 export default Card;
--- src/theme/mainTheme.jsx
+++ src/theme/mainTheme.jsx
@@ -1,7 +1,7 @@
 export const theme = {
-  primary: "#ffd82b",
-  secondary: "#8ad8f4",
-  tertiary: "#8cce78",
+  note: "#ffd82b",
+  twitter: "#8ad8f4",
+  article: "#8cce78",
   grey100: "#f5f5f5",
   grey200: "#e6e6e6",
   black: "#000000",
